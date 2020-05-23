4ffd760056b187b87dc200c3ad3f9a8ad86f9eae
Daniel Stout <ospreythirtyone@protonmail.com>
2020-05-23 17:00:58 -0400

    circleci setup

--- spec/index.js
+++ spec/index.js
@@ -2,22 +2,6 @@ const { webkit } = require('playwright');
 const { expect } = require("chai");
 const internet = require('../pages/page.js');
 
-// (async () => {
-//     // const browser = await webkit.launch({ headless: false, slowMo: 50 });
-//     const browser = await webkit.launch();
-//     const page = await browser.newPage();
-//     await page.goto(internet.url());
-//     // for (const link in internet.links) {
-//     //     await page.click(internet.links[link]);
-//     //     // console.log(internet.links().aBTesting)
-//     //     await page.screenshot({ path: `screenshots/${link}.png` });
-//     //     await page.goto(internet.url());
-//     // }
-//     // await page.click(links.aBTesting);
-//     // await page.screenshot({ path: `screenshots/example.png` });
-//     await browser.close();
-// })();
-// console.log(internet.Index.url());
 let page;
 let browser;
 describe("the internet", () => {
@@ -43,13 +27,9 @@ describe("the internet", () => {
         }
       });
     it("should reach the internet test page", async () => {
-        // await page.goto(internet.url());
-        // console.log(internet.links.aBTesting)
         await page.click(internet.links.wysiwygEditor);
-        await page.screenshot({ path: `screenshots/someshit.png` });
         const title = await page.$eval("h3", (el) => el.textContent);
         expect(title).to.equal('An iFrame containing the TinyMCE WYSIWYG Editor');
         expect((await page.title()).toLowerCase()).to.equal("the internet");
-        // expect(true).to.equal(false);
     });
 });
